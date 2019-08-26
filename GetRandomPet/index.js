b16158ca93cadf2fbb74a2c4754c00987d149870
Linda Nichols <lynnaloo@gmail.com>
2019-08-25 23:50:20 -0400

    [debug] Add more information to response

--- GetRandomPet/index.js
+++ GetRandomPet/index.js
@@ -23,6 +23,11 @@ module.exports = function(context, data) {
     const adoptionBlurb = `${util.getAdoptionText(animalFacts.animalName, animalFacts.animalGender, animalFacts.animalBreed, animalFacts.animalUrl, animalFacts.animalCity, animalFacts.animalState)}`;
     console.log('DEBUG:', adoptionBlurb);
 
+    context.bindings.myOutput = {
+      text: adoptionBlurb,
+      photo: animalFacts.animalPhoto
+    };
+
     context.res = {
       body: adoptionBlurb
     };
